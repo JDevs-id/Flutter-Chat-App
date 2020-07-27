3f514d86cd4dfb23e966547193de463c413f4e12
JDevs-id <57583609+JDevs-id@users.noreply.github.com>
2020-07-27 12:35:32 +0700

    Update addUser.php
    Penggantian query

--- lib/back-end/addUser.php
+++ lib/back-end/addUser.php
@@ -3,5 +3,6 @@ include 'conn.php';
 
 $username = $_POST['username'];
 $password = $_POST['password'];
+$status = $_POST['status'];
 
-$conn->query("INSERT INTO `tb_user` (`username`, `password`) VALUES ('$username', '$password')");
+$conn->query("INSERT INTO `tb_user` (`username`, `password`, `status`) VALUES ('$username', '$password', '$status')");
