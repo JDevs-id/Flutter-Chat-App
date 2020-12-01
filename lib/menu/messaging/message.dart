71eaebb1d4e79302d860a42d7e6a6bd65ebb0d9f
JDevs-ID <jdevs.id@gmail.com>
2020-12-01 18:52:25 +0700

    penambahan form pesan

--- lib/menu/messaging/message.dart
+++ lib/menu/messaging/message.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:user_management/global/color.dart';
 
 class Message extends StatefulWidget {
@@ -11,12 +12,82 @@ class Message extends StatefulWidget {
 class _MessageState extends State<Message> {
   @override
   Widget build(BuildContext context) {
+    void _send() {
+      SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
+      FocusScope.of(context).unfocus();
+    }
+
     return Scaffold(
       backgroundColor: PrimaryColor,
-      body: Center(
-        child: Text(
-          "Menu Messaging",
-          style: TextStyle(color: SecondaryColor),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        child: Stack(
+          children: <Widget>[
+            Align(
+                alignment: Alignment.topCenter,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 60),
+                  child: Icon(
+                    Icons.message,
+                    color: SecondaryColor.withOpacity(0.3),
+                    size: MediaQuery.of(context).size.width * 0.9,
+                  ),
+                )),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  TextField(
+                    textInputAction: TextInputAction.next,
+                    onSubmitted: (_) => FocusScope.of(context).nextFocus(),
+                    style: TextStyle(color: SecondaryColor),
+                    inputFormatters: [LengthLimitingTextInputFormatter(20)],
+                    decoration: InputDecoration(
+                        labelText: "to",
+                        labelStyle: TextStyle(color: SecondaryColor),
+                        hintText: "Input the destination username",
+                        hintStyle:
+                            TextStyle(color: SecondaryColor.withOpacity(0.5)),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: SecondaryColor.withOpacity(0.8)))),
+                  ),
+                  TextField(
+                    textInputAction: TextInputAction.go,
+                    onSubmitted: (_) => _send(),
+                    maxLines: null,
+                    style: TextStyle(color: SecondaryColor),
+                    decoration: InputDecoration(
+                        labelText: "Message",
+                        labelStyle: TextStyle(color: SecondaryColor),
+                        hintText: "Input your message",
+                        hintStyle:
+                            TextStyle(color: SecondaryColor.withOpacity(0.5)),
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                                color: SecondaryColor.withOpacity(0.8)))),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(40.0),
+                      child: FlatButton(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          color: SecondaryColor,
+                          child: Text(
+                            "Send",
+                            style: TextStyle(
+                                color: PrimaryColor,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          onPressed: () => _send()),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
     );
